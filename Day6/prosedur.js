7463665c2750821dbf0528e7e0bb48199893eca6
cutnurhidayanti <cutnurhidayanti03@gmail.com>
2025-08-08 11:13:39 +0700

    add folder day 6 function dan prosedure

---
+++ Day6/prosedur.js
@@ -0,0 +1,8 @@
+function cetakBiodata(nama, umur, hobi, alamat) {
+    console.log("Nama :", nama)
+    console.log("umur :", umur)
+    console.log("hobi :", hobi)
+    console.log("alamat :", alamat)
+}
+
+cetakBiodata("yanti", 20, "roblox", "blangbintang")
